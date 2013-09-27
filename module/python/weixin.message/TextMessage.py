84a06915b3ca5bcf789dc27276cee40f0fb23f8c
usoluyun <usoluyun@gmail.com>
2013-09-27 14:13:10 +0800

    modify name convention according to PEP8

--- module/python/weixin.message/TextMessage.py
+++ module/python/weixin.message/TextMessage.py
@@ -2,31 +2,35 @@
 #Filename: 
 #author: Chester LU <usoluyun@gmail.com>
 
-import os,sys
+import sys
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ParseError
+
 
 class TextMessage(object):
-    '''
-    The weixin text message adaptor
-    '''
-    def __init__(self, testmsg):
-        try:
-            self.ToUserName   = testmsg.find('ToUserName').text
-            self.FromUserName = testmsg.find('FromUserName').text
-            self.CreateTime   = testmsg.find('CreateTime').text
-            self.MsgType      = testmsg.find('MsgType').text
-            self.MsgId        = testmsg.find('MsgId').text
-            self.Content      = testmsg.find('Content').text
-        except:
-            print "Unexptected error: " ,  sys.exc_info()[0]
+    def __init__(self, test_msg):
+        """
 
-if __name__=="__main__":
-    with open('/Users/usoluyun/Nutstore/weixin/xml/msg_text_example.xml','r') as d:
-        testmsg = TextMessage(ElementTree.parse(d))
-        print testmsg.ToUserName
-        print testmsg.FromUserName
-        print testmsg.CreateTime
-        print testmsg.MsgType
-        print testmsg.MsgId
-        print testmsg.Content
+        @rtype : xml object
+        """
+        try:
+            self.to_username = test_msg.find('ToUserName').text
+            self.from_username = test_msg.find('FromUserName').text
+            self.create_time = test_msg.find('CreateTime').text
+            self.msg_type = test_msg.find('MsgType').text
+            self.msg_id = test_msg.find('MsgId').text
+            self.content = test_msg.find('Content').text
+        except ParseError:
+            print "Unexpected error: ", sys.exc_info()[0]
+
+
+if __name__ == "__main__":
+    with open('../../../xml/msg_text_example.xml', 'r') as d:
+        test_msg = TextMessage(ElementTree.parse(d))
+        print test_msg.to_username
+        print test_msg.from_username
+        print test_msg.create_time
+        print test_msg.msg_type
+        print test_msg.msg_id
+        print test_msg.content
 
